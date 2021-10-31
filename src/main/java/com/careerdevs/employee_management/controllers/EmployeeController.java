60a2565b4e4a06d1bb06d71e524cb8e65943dbdd
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-10-31 13:10:19 -0400

    added mysql address and autowird repository

--- src/main/java/com/careerdevs/employee_management/controllers/EmployeeController.java
+++ src/main/java/com/careerdevs/employee_management/controllers/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.careerdevs.employee_management.controllers;
 import com.careerdevs.employee_management.entities.Employee;
 import com.careerdevs.employee_management.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,8 +13,11 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    @Autowired
     private EmployeeRepository repository;
+
     private AtomicLong idCounter = new AtomicLong();
+
 
     public EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
